294d39a72f7cade83feb5b97029650349c120840
pldigitalg-ui <pldigitalg@gmail.com>
2026-04-03 10:27:51 -0300

    Create scroll-top.js

---
+++ Js/Modules/scroll-top.js
@@ -0,0 +1,19 @@
+export function initScrollTop() {
+  const button = document.getElementById("scrollTopBtn");
+  if (!button) return;
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 500) {
+      button.classList.add("show");
+    } else {
+      button.classList.remove("show");
+    }
+  });
+
+  button.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+}
