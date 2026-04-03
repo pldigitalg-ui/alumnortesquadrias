62159ce946681a9a8cf210949edbaaeac591453d
pldigitalg-ui <pldigitalg@gmail.com>
2026-04-03 17:32:28 -0300

    Update lightbox.js

--- Js/Modules/lightbox.js
+++ Js/Modules/lightbox.js
@@ -1,29 +1,10 @@
-export default function initLightbox() {
-  const cards = document.querySelectorAll('.project-card');
-  const modal = document.getElementById('lightbox');
-  const img = document.getElementById('lightboxImage');
-  const title = document.getElementById('lightboxTitle');
-  const desc = document.getElementById('lightboxDescription');
+document.querySelectorAll('.lightbox').forEach(img=>{
+  img.onclick = ()=>{
+    let overlay = document.createElement('div');
+    overlay.innerHTML = `<img src="${img.src}">`;
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
 
-  if (!cards.length) return;
-
-  cards.forEach(card => {
-    card.addEventListener('click', () => {
-      img.src = card.dataset.lightboxImage;
-      title.textContent = card.dataset.lightboxTitle;
-      desc.textContent = card.dataset.lightboxDescription;
-
-      modal.classList.add('active');
-    });
-  });
-
-  modal.addEventListener('click', (e) => {
-    if (e.target.dataset.lightboxClose !== undefined || e.target.id === 'lightbox') {
-      modal.classList.remove('active');
-    }
-  });
-
-  document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape') modal.classList.remove('active');
-  });
-}
+    overlay.onclick = ()=>overlay.remove();
+  }
+});
