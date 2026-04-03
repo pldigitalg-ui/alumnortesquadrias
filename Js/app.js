0ab20b4e626585135bf46f93490b97dff4209ff3
pldigitalg-ui <pldigitalg@gmail.com>
2026-04-03 17:21:03 -0300

    Update app.js

--- Js/app.js
+++ Js/app.js
@@ -1,15 +1,17 @@
-import { initMenu } from "./modules/menu.js";
-import { initHero } from "./modules/hero.js";
-import { initReveal } from "./modules/reveal.js";
-import { initCounter } from "./modules/counter.js";
-import { initLightbox } from "./modules/lightbox.js";
-import { initContactModal } from "./modules/contact-modal.js";
-import { initScrollTop } from "./modules/scroll-top.js";
+import initMenu from './modules/menu.js';
+import initHero from './modules/hero.js';
+import initReveal from './modules/reveal.js';
+import initCounter from './modules/counter.js';
+import initLightbox from './modules/lightbox.js';
+import initContactModal from './modules/contact-modal.js';
+import initScrollTop from './modules/scroll-top.js';
 
-initMenu();
-initHero();
-initReveal();
-initCounter();
-initLightbox();
-initContactModal();
-initScrollTop();
+document.addEventListener('DOMContentLoaded', () => {
+  initMenu();
+  initHero();
+  initReveal();
+  initCounter();
+  initLightbox();
+  initContactModal();
+  initScrollTop();
+});
